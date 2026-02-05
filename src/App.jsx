d38091dea64124a839a920aa5adb8c8e034edf52
I-Maged <magedhanafy@gmail.com>
2026-02-05 21:57:38 +0200

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Form from './components/Form'
 
 import { CitiesProvider } from './contexts/CitiesContext'
 import { AuthProvider } from './contexts/FakeAuthContext'
+import ProtectedRoute from './pages/ProtectedRoute'
 
 const App = () => {
   return (
@@ -25,7 +26,14 @@ const App = () => {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
 
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to='cities' />} />
               <Route path='cities' element={<CityList />} />
               <Route path='cities/:id' element={<City />} />
